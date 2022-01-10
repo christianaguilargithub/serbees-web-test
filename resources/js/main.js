51b491737c81d6140c13b02320fe96292ea4af6f
christianaguilargithub <aguilar.chris9123@gmail.com>
2022-01-10 19:50:26 +0800

    [ UPDATE ] - client page js

--- resources/js/main.js
+++ resources/js/main.js
@@ -28,3 +28,13 @@ $(document).ready(() => {
         templateResult: serviceFormatState,
     });
 });
+
+
+function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(cityName).style.display = "block";
+  }
