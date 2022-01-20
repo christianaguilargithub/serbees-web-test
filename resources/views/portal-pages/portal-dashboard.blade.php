cfc5679521eef6578722d599a8b76e21e7f4fa47
christianaguilargithub <aguilar.chris9123@gmail.com>
2022-01-20 18:22:49 +0800

    no message

--- resources/views/portal-pages/portal-dashboard.blade.php
+++ resources/views/portal-pages/portal-dashboard.blade.php
@@ -2,9 +2,5 @@
 @extends('layouts.portal')
 
 @section('content')
-    Dashboard
-
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi debitis, facere deleniti quidem 
-    voluptatum, cumque maxime incidunt minus saepe maiores, ullam dolore accusamus cupiditate vero 
-    totam? Aperiam amet maiores accusamus.
+    
 @endsection
