4d17ab037a80ea138fd7b66bb1fbdabe9538754a
christianaguilargithub <aguilar.chris9123@gmail.com>
2022-01-24 15:59:59 +0800

    no message

--- resources/views/portal-pages/portal-provider.blade.php
+++ resources/views/portal-pages/portal-provider.blade.php
@@ -1,4 +1,145 @@
+
 @extends('layouts.portal')
+<link rel="stylesheet" href="{{ asset('css/portal-provider.css') }}" type="text/css">
 @section('content')
-    
+
+<div class="overall-portal-provider">
+    <div class="provider-list-container">
+      <div class="portal-provider-container">
+        <div class="provider-status">
+            <div class="total-provider">
+                <p class="portal-provider-status">Total Clients</p>
+                <p class="portal-provider-status-count">326</p>
+            </div>
+            <div class="register-today">
+                <p class="portal-provider-status">Registered Today</p>
+                <p class="portal-provider-status-count">20</p>
+            </div>
+            <div class="register-today">
+                <p class="portal-provider-status">Pending Payments</p>
+                <p class="portal-provider-status-count">326</p>
+            </div>
+            <div class="register-today">
+                <p class="portal-provider-status">Pending</p>
+                <p class="portal-provider-status-count">20</p>
+            </div>
+        </div>
+    </div>
+        <div class="provider-list-menu">
+            <div class="provider-list-number-sort-container">
+                <label for="numberSelect">Show </label>
+                <select class="select-box" name="numberSelect" id="numberSelect">
+                    <option style="width:60px" value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select>
+            </div>
+            <div class="provider-list-search-container">
+                <input data-provide="typeahead" data-items="4"  type="text" class="search-query">
+            </div>
+        </div>
+        <div class="provider-list">
+            <table id="customers" cellspacing="0" cellpadding="0">
+                <tr style="border-bottom: 1px solid #ddd;">
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Service</th>
+                  <th>Mobile Number</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Maria Anders</td>
+                  <td>mariaanders@email.com</td>
+                  <td>09051234567</td>
+                  <td><div class="status-box-pending">Pending Requirements</div></td>
+                  <td><a href="">View details</a></td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Christina Berglund</td>
+                  <td>mariaanders@email.com</td>
+                  <td>09051234567</td>
+                  <td><div class="status-box-approved">Pending Requirements</div></td>
+                  <td><a href="">View details</a></td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Francisco Chang</td>
+                  <td>mariaanders@email.com</td>
+                  <td>09051234567</td>
+                  <td><div class="status-box-pending">Pending Requirements</div></td>
+                  <td><a href="">View details</a></td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>Roland Mendel</td>
+                  <td>mariaanders@email.com</td>
+                  <td>09051234567</td>
+                  <td><div class="status-box-pending">Pending Requirements</div></td>
+                  <td><a href="">View details</a></td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>Helen Bennett</td>
+                  <td>mariaanders@email.com</td>
+                  <td>09051234567</td>
+                  <td><div class="status-box-pending">Pending Requirements</div></td>
+                  <td><a href="">View details</a></td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>Philip Cramer</td>
+                  <td>mariaanders@email.com</td>
+                  <td>09051234567</td>
+                  <td><div class="status-box-pending">Pending Requirements</div></td>
+                  <td><a href="">View details</a></td>
+                </tr>
+                <tr>
+                  <td>7</td>
+                  <td>Yoshi Tannamuri</td>
+                  <td>mariaanders@email.com</td>
+                  <td>09051234567</td>
+                  <td><div class="status-box-pending">Pending Requirements</div></td>
+                  <td><a href="">View details</a></td>
+                </tr>
+                <tr>
+                  <td>8</td>
+                  <td>Giovanni Rovelli</td>
+                  <td>mariaanders@email.com</td>
+                  <td>09051234567</td>
+                  <td><div class="status-box-pending">Pending Requirements</div></td>
+                  <td><a href="">View details</a></td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>Simon Crowther</td>
+                  <td>mariaanders@email.com</td>
+                  <td>09051234567</td>
+                  <td><div class="status-box-pending">Pending Requirements</div></td>
+                  <td><a href="">View details</a></td>
+                </tr>
+                <tr>
+                  <td>10</td>
+                  <td>Marie Bertrand</td>
+                  <td>mariaanders@email.com</td>
+                  <td>09051234567</td>
+                  <td><div class="status-box-pending">Pending Requirements</div></td>
+                  <td><a href="">View details</a></td>
+                </tr>
+              </table>
+        </div>
+    </div>
+</div>
+
+
+
 @endsection
