42f05c7a626928030bcba8e6d0838ea0e29d23f1
christianaguilargithub <aguilar.chris9123@gmail.com>
2022-02-02 16:43:40 +0800

    no message

--- resources/views/portal-pages/portal-bookings.blade.php
+++ resources/views/portal-pages/portal-bookings.blade.php
@@ -1,9 +1,78 @@
 
 @extends('layouts.portal')
 <link rel="stylesheet" href="{{ asset('css/portal-bookings.css') }}" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 @section('content')
 
 <div class="overall-portal-provider">
+
+  <div class="modal-container2" id="modal_container2">
+    <div class="modal2">
+      <div class="client-details-title">
+        <p style="font-size: 18px; margin:0px; font-weight:700;">Booking Details</p> <p style="font-size: 18px; margin:0px; font-weight:700;cursor: pointer;" id="close2">&#215;</p> 
+      </div>
+      <div class="client-details-container">
+        <div class="client-details">
+          <div class="client-details-name-1">
+            <p style="margin-bottom:0px;font-size:16px;">Booking Number 304</p> 
+              <div style="display: flex;flex-direction:row; justify-content:center;align-content:center;"><div class="status-box-active-1">Completed</div></div>
+          </div>
+          <p style="text-align:left; margin: 0px 0px 10px 0px; font-weight:700;font-size:30px">Angelica Marie Abinal</p>
+          <p style="text-align:left; margin: 0px 0px 20px 5px;">Blk 5, Lot 1 285 San Guillermo Street Putatan</p>
+          <table style="width:100%;line-height:100%; border-top: 1px solid #C4C4C4;">
+            
+            <tr>
+              <th style="padding-top: 20px;">Service</th>
+              <td style="padding-top: 20px;">Veterinary Services</td>
+            </tr>
+            <tr>
+              <th>Provider</th>
+              <td>Michael Anthony de castro</td>
+            </tr>
+            <tr>
+              <th>Start date</th>
+              <td>Aug 16, 2021 03:00:00 PM</td>
+            </tr>
+            <tr>
+              <th>End date</th>
+              <td>Aug 16, 2021 11:59:59 PM</td>
+            </tr>
+            <tr>
+              <th>Started at</th>
+              <td>N/A</td>
+            </tr>
+            <tr>
+              <th>Ended at</th>
+              <td>N/A</td>
+            </tr>
+            <tr>
+              <th>Duration</th>
+              <td>N/A</td>
+            </tr>
+            <tr>
+              <th>Rating</th>
+              <td>
+                <div>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>Client Message</th>
+            </tr>
+            
+          </table>
+          <p style="text-align:left; margin: 0px 0px 20px 5px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a mollis elit, vel bibendum libero.</p>
+          <button id="close1" style="border: 1px solid #C4C4C4; width: 540px; height: 50px; background-color: #FFF; color:#6A6A6A; border-radius: 4px; margin-right:7.5px;margin-bottom:20px;">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
     <div class="provider-list-container">
       <div class="portal-provider-container">
         <div class="provider-status">
@@ -61,7 +130,7 @@
                   <td>Maria Anders</td>
                   <td>Serbees Account</td>
                   <td><div class="status-box-completed">Completed by the System</div></td>
-                  <td><a href="">View details</a></td>
+                  <td><p id="open1">View details</p></td>
                 </tr>
                 <tr>
                   <td>2</td>
@@ -69,7 +138,7 @@
                   <td>Maria Anders</td>
                   <td>Serbees Account</td>
                   <td><div class="status-box-completed">Completed</div></td>
-                  <td><a href="">View details</a></td>
+                  <td><p id="open2">View details</p></td>
                 </tr>
                 <tr>
                   <td>3</td>
@@ -77,7 +146,7 @@
                   <td>Maria Anders</td>
                   <td>Serbees Account</td>
                   <td><div class="status-box-cancelled">Cancelled by client</div></td>
-                  <td><a href="">View details</a></td>
+                  <td><p id="open3">View details</p></td>
                 </tr>
                 <tr>
                   <td>4</td>
@@ -85,7 +154,7 @@
                   <td>Maria Anders</td>
                   <td>Serbees Account</td>
                   <td><div class="status-box-completed">Completed by the System</div></td>
-                  <td><a href="">View details</a></td>
+                  <td><p id="open4">View details</p></td>
                 </tr>
                 <tr>
                   <td>5</td>
@@ -93,7 +162,7 @@
                   <td>Maria Anders</td>
                   <td>Serbees Account</td>
                   <td><div class="status-box-completed">Completed by the System</div></td>
-                  <td><a href="">View details</a></td>
+                  <td><p id="open5">View details</p></td>
                 </tr>
                 <tr>
                   <td>6</td>
@@ -101,7 +170,7 @@
                   <td>Maria Anders</td>
                   <td>Serbees Account</td>
                   <td><div class="status-box-completed">Completed by the System</div></td>
-                  <td><a href="">View details</a></td>
+                  <td><p id="open6">View details</p></td>
                 </tr>
                 <tr>
                   <td>7</td>
@@ -109,7 +178,7 @@
                   <td>Maria Anders</td>
                   <td>Serbees Account</td>
                   <td><div class="status-box-completed">Completed by the System</div></td>
-                  <td><a href="">View details</a></td>
+                  <td><p id="open7">View details</p></td>
                 </tr>
                 <tr>
                   <td>8</td>
@@ -117,7 +186,7 @@
                   <td>Maria Anders</td>
                   <td>Serbees Account</td>
                   <td><div class="status-box-completed">Completed by the System</div></td>
-                  <td><a href="">View details</a></td>
+                  <td><p id="open8">View details</p></td>
                 </tr>
                 <tr>
                   <td>9</td>
@@ -125,7 +194,7 @@
                   <td>Maria Anders</td>
                   <td>Serbees Account</td>
                   <td><div class="status-box-completed">Completed by the System</div></td>
-                  <td><a href="">View details</a></td>
+                  <td><p id="open9">View details</p></td>
                 </tr>
                 <tr>
                   <td>10</td>
@@ -133,7 +202,7 @@
                   <td>Maria Anders</td>
                   <td>Serbees Account</td>
                   <td><div class="status-box-completed">Completed by the System</div></td>
-                  <td><a href="">View details</a></td>
+                  <td><p id="open10">View details</p></td>
                 </tr>
               </table>
         </div>
@@ -141,5 +210,99 @@
 </div>
 
 
+<script>
+  const open1 = document.getElementById('open1');
+  const open2 = document.getElementById('open2');
+  const open3 = document.getElementById('open3');
+  const open4 = document.getElementById('open4');
+  const open5 = document.getElementById('open5');
+  const open6 = document.getElementById('open6');
+  const open7 = document.getElementById('open7');
+  const open8 = document.getElementById('open8');
+  const open9 = document.getElementById('open9');
+  const open10 = document.getElementById('open10');
+  const modal_container1 = document.getElementById('modal_container1');
+  const modal_container2 = document.getElementById('modal_container2');
+  const modal_container3 = document.getElementById('modal_container3');
+  const modal_container4 = document.getElementById('modal_container4');
+  const modal_container5 = document.getElementById('modal_container5');
+  const modal_container6 = document.getElementById('modal_container6');
+  const modal_container7 = document.getElementById('modal_container7');
+  const modal_container8 = document.getElementById('modal_container8');
+  const modal_container9 = document.getElementById('modal_container9');
+  const modal_container10 = document.getElementById('modal_container10');
+  const close1 = document.getElementById('close1');
+  const close2 = document.getElementById('close2');
+  const close3 = document.getElementById('close3');
+  const close4 = document.getElementById('close4');
+  const close5 = document.getElementById('close5');
+  const close6 = document.getElementById('close6');
+  const close7 = document.getElementById('close7');
+  const close8 = document.getElementById('close8');
+  const close9 = document.getElementById('close9');
+  const close10 = document.getElementById('close10');
+
+  open1.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+  })
+  open2.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+  })
+  open3.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+  })
+  open4.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+  })
+  open5.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+  })
+  open6.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+  })
+  open7.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+  })
+  open8.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+  })
+  open9.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+  })
+  open10.addEventListener('click', () => {
+    modal_container2.classList.add('show');
+  })
+
+  close1.addEventListener('click', () => {
+    modal_container2.classList.remove('show');
+  })
+  close2.addEventListener('click', () => {
+    modal_container2.classList.remove('show');
+  })
+  // close3.addEventListener('click', () => {
+  //   modal_container2.classList.remove('show');
+  // })
+  // close4.addEventListener('click', () => {
+  //   modal_container2.classList.remove('show');
+  // })
+  // close5.addEventListener('click', () => {
+  //   modal_container2.classList.remove('show');
+  // })
+  // close6.addEventListener('click', () => {
+  //   modal_container2.classList.remove('show');
+  // })
+  // close7.addEventListener('click', () => {
+  //   modal_container2.classList.remove('show');
+  // })
+  // close8.addEventListener('click', () => {
+  //   modal_container2.classList.remove('show');
+  // })
+  // close9.addEventListener('click', () => {
+  //   modal_container2.classList.remove('show');
+  // })
+  // close10.addEventListener('click', () => {
+  //   modal_container2.classList.remove('show');
+  // })
+</script>
 
 @endsection
