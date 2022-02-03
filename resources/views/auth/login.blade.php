ff02f8659fe20e570b0b3f034169bc0dcbc35567
christianaguilargithub <aguilar.chris9123@gmail.com>
2022-02-03 20:02:20 +0800

    no message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,20 +9,23 @@
 @endsection
 <x-guest-layout>
     <div class="top-page">
-        <form action="" class="login-form-container">
+        <form action="{{ route('client.auth') }}" method="POST" class="login-form-container">
+            @csrf
+
             <div class="login-form-content">
                 <br>
                 <label style="font-family: cocogoose; font-weight:600; color:white; font-size:30px">LOGIN & <br>BOOK NOW!</label>
                 <br>
                 <label style="font-family: Roboto; font-weight:700">User Name
                 </label>
-                <input type="text" name="Uname" id="Uname" placeholder="Username" class="login-input">
+                <input type="text" name="user_name" id="Uname" placeholder="Username" class="login-input">
                 <br>
                 <label style="font-family: Roboto; font-weight:700">Password
                 </label>
-                <input type="Password" name="Pass" id="Pass" placeholder="Password" class="login-input">
+                <input type="password" name="password" id="myInput" placeholder="Password" class="login-input" >
                 <br>
-                <input type="button" name="log" id="log" value="Log In" style="width: 150px;font-family:roboto; border-radius:10px;color:white;background-color:#1E90FF;box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+                <span style="font-family: Roboto; font-weight:700;padding-bottom: 20px;margin-top:-20px;"><input style="margin-bottom:3px;margin-right:5px; border-radius:4px;"type="checkbox" onclick="myFunction()">Show Password</span>
+                <input type="submit" name="log" id="log" value="Log In" style="width: 150px;font-family:roboto; border-radius:10px;color:white;background-color:#1E90FF;box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
                 backdrop-filter: blur( 4px );
                 -webkit-backdrop-filter: blur( 4px ); margin-left:auto;margin-right:auto;">
                 <br>
@@ -30,12 +33,21 @@
                 backdrop-filter: blur( 4px );
                 -webkit-backdrop-filter: blur( 4px ); margin-left:auto;margin-right:auto;">
                 <br>
-                <span style="font-family: Roboto; font-weight:700"><input type="checkbox" id="check">
-                Remember me</span>
+                <span style="font-family: Roboto; font-weight:700"><input type="checkbox" id="check" style="margin-bottom:3px;margin-right:5px; border-radius:4px;">Remember me</span>
                 <br>
                 <span style="font-family: Roboto; font-weight:700">Forgot <a href="#">Password</a></span>
                 <br>
             </div>
         </form>
     </div>
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myInput");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
+    </script>
 </x-guest-layout>
